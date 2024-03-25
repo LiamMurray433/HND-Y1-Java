597894c356ca797f99f1ad7061b58eae95560786
Liam Murray <l.murray8391@gmail.com>
2024-03-25 15:58:27 +0000

    Added admin and main class

---
+++ crufts-app/src/Admin.java
@@ -0,0 +1,43 @@
+public class Admin {
+
+    private static int adminCounter = 0;
+    private String adminID;
+    private String username;
+    private String password;
+
+
+    // Constructor
+    public Admin(String username, String password){
+        this.adminID = String.format("%02d", ++adminCounter);
+        this.username = username;
+        this.password = password;
+
+    }
+
+    // Getters
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    // Setters
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "adminID='" + adminID + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
--- crufts-app/src/Main.java
+++ crufts-app/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Variables
+        String username;
+        String password;
+
+        // Adding a new admin
+        Admin admin = new Admin("Liam", "1111");
+        System.out.println(admin.toString());
+        username = "Liam";
+        password = "1111";
+        boolean userExists = checkUserCredentials(username, password, admin);
+        // Check
+        if(userExists){
+            System.out.println("You are logged in");
+        }
+        else{
+            System.out.println("Wrong username or password");
+        }
+
+        // Adding new Breed
+        Breed husky = new Breed("Husky");
+        System.out.println(husky.toString());
+
+    }
+    private static boolean checkUserCredentials(String username,String password, Admin admin){
+        return(admin.getUsername().equals(username)) && (admin.getPassword().equals(password));
     }
 }
